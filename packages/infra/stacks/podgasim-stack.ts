d1b3503b1128ec019b46b4ca7c646f88c2ed793f
Nate Smith <nate@falconlaboratories.io>
2022-06-17 22:06:49 -0500

    made a todo list

--- packages/infra/stacks/podgasim-stack.ts
+++ packages/infra/stacks/podgasim-stack.ts
@@ -1,3 +1,17 @@
+/* 
+  TODO:
+    - Sort out the A records, domain, certs, hosted zone, dns stuff
+    - Create the lambda Funcs
+    - Setup AWS Vault
+    - Integrate dotenv somehow ... or something similar
+    - Decide on a naming convention
+    - Pass in variables and name everything
+    - Bootstrap the CDK
+    - Try to deploy once
+    - Set the nameservers on hover
+    - Setup Pipeline deploy with Github actions
+*/
+
 import * as path from "path";
 
 import {
@@ -12,13 +26,18 @@ import { Construct } from "constructs";
 import {
   PublicHostedZone,
   ZoneDelegationRecord,
+  ARecord,
+  RecordTarget,
+  HostedZone,
 } from "aws-cdk-lib/aws-route53";
 import {
   DnsValidatedCertificate,
+  Certificate,
   ValidationMethod,
 } from "aws-cdk-lib/aws-certificatemanager";
+import { ApiGatewayv2DomainProperties } from "aws-cdk-lib/aws-route53-targets";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { LambdaRestApi, HttpIntegration } from "aws-cdk-lib/aws-apigateway";
+// import { LambdaRestApi, HttpIntegration } from "aws-cdk-lib/aws-apigateway";
 
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
@@ -34,20 +53,80 @@ import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-al
 // https://dev.to/aws-builders/automate-building-a-unique-domain-hosting-environment-with-aws-cdk-1dd1
 
 // THIS IS IT NATE: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_apigateway-readme.html#custom-domains
-export class DNSStack extends NestedStack {
-  domain: string;
+// export class DNSStack extends NestedStack {
+//   domain: string;
 
-  constructor(scope: Construct, id: string, props?: NestedStackProps) {
+//   constructor(scope: Construct, id: string, props?: NestedStackProps) {
+//     super(scope, id, props);
+
+//     const apexDomain = "podgasim.com";
+//     // const domain = `picture.${apexDomain}`;
+//     this.domain = apexDomain;
+
+//     // as above we create a hostedzone for the subdomain
+//     const hostedZone = new PublicHostedZone(this, "PublicHostedZone", {
+//       zoneName: this.domain,
+//     });
+//     // add a ZoneDelegationRecord so that requests for *.picture.bahr.dev
+//     // and picture.bahr.dev are handled by our newly created HostedZone
+//     const nameServers: string[] = hostedZone.hostedZoneNameServers!;
+//     const rootZone = PublicHostedZone.fromLookup(this, "Zone", {
+//       domainName: apexDomain,
+//     });
+//     new ZoneDelegationRecord(this, "Delegation", {
+//       recordName: this.domain,
+//       nameServers,
+//       zone: rootZone,
+//       ttl: Duration.minutes(1),
+//     });
+
+//     const certificate = new DnsValidatedCertificate(this, "Certificate", {
+//       region: "us-east-1",
+//       hostedZone: hostedZone,
+//       domainName: this.domain,
+//       subjectAlternativeNames: [`*.${this.domain}`],
+//       // validationDomains: {
+//       //   [this.domain]: this.domain,
+//       //   [`*.${this.domain}`]: this.domain,
+//       // },
+//       // validationMethod: ValidationMethod.DNS,
+//     });
+//   }
+// }
+
+declare const dn: DomainName;
+declare const eventtFn: NodejsFunction;
+
+interface CDKContext {
+  appName: string;
+  region: string;
+  environment: string;
+  branchName: string;
+  accountNumber: string;
+  baseDomain: string;
+  apiDomain: string;
+  hostedZoneId: string;
+  regionalCertArn: string;
+}
+
+interface APIProps extends NestedStackProps {
+  domain: string;
+}
+class APIStack extends NestedStack {
+  readonly domain: string;
+
+  constructor(scope: Construct, id: string, props?: APIProps) {
     super(scope, id, props);
 
     const apexDomain = "podgasim.com";
-    // const domain = `picture.${apexDomain}`;
+    //     // const domain = `picture.${apexDomain}`;
     this.domain = apexDomain;
 
     // as above we create a hostedzone for the subdomain
     const hostedZone = new PublicHostedZone(this, "PublicHostedZone", {
       zoneName: this.domain,
     });
+
     // add a ZoneDelegationRecord so that requests for *.picture.bahr.dev
     // and picture.bahr.dev are handled by our newly created HostedZone
     const nameServers: string[] = hostedZone.hostedZoneNameServers!;
@@ -72,16 +151,37 @@ export class DNSStack extends NestedStack {
       // },
       // validationMethod: ValidationMethod.DNS,
     });
-  }
-}
-declare const dn: DomainName;
-declare const eventtFn: NodejsFunction;
-interface APIProps extends NestedStackProps {
-  domain: string;
-}
-class APIStack extends NestedStack {
-  constructor(scope: Construct, id: string, props?: APIProps) {
-    super(scope, id, props);
+
+    // Define API Authorizer
+    // const apiAuthorizer = new HttpLambdaAuthorizer(
+    //   "apiAuthorizer",
+    //   props.lambdaFunctions["api-authorizer"],
+    //   {
+    //     authorizerName: `${context.appName}-http-api-authorizer-${context.environment}`,
+    //     responseTypes: [HttpLambdaResponseType.SIMPLE],
+    //   }
+    // );
+
+    // Define Custom Domain
+    const apiDomain = new DomainName(this, "apiDomain", {
+      domainName: this.domain,
+      certificate: certificate,
+    });
+
+    // Add Route 53 entry for Api
+    new ARecord(this, "apiDNSRecord", {
+      zone: HostedZone.fromHostedZoneAttributes(this, "apiHostedZone", {
+        hostedZoneId: hostedZone.hostedZoneId,
+        zoneName: hostedZone.zoneName,
+      }),
+      recordName: this.domain,
+      target: RecordTarget.fromAlias(
+        new ApiGatewayv2DomainProperties(
+          apiDomain.regionalDomainName,
+          apiDomain.regionalHostedZoneId
+        )
+      ),
+    });
 
     const httpApi = new HttpApi(this, "HttpApi", {
       defaultDomainMapping: {
@@ -127,7 +227,9 @@ export class PodgasimStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const { domain } = new DNSStack(this, "dns-stack");
+    const domain = "podgasim.com";
+
+    // const { domain } = new DNSStack(this, "dns-stack");
     new APIStack(this, "api-stack", { domain });
 
     // new CfnOutput(this, "APIURL", {
